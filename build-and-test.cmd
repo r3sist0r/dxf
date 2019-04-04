1c663979a2e0ef5872e6682c48f5c8d6e0a03571
Brett V. Forsgren <brett.forsgren@outlook.com>
2019-04-04 13:24:24 -0700

    auto-detect when to re-run the generator

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,32 +1,16 @@
 @echo off
 
-:: run code generator
-set GENERATOR_DIR=%~dp0src\IxMilia.Dxf.Generator
-set LIBRARY_DIR=%~dp0src\IxMilia.Dxf
-pushd "%GENERATOR_DIR%"
-dotnet restore
-if errorlevel 1 goto error
-dotnet build
-if errorlevel 1 goto error
-dotnet run "%LIBRARY_DIR%"
-if errorlevel 1 goto error
-popd
+set Solution=%~dp0src\IxMilia.Dxf.sln
 
-:: build library
-set LIBRARY_PROJECT=%LIBRARY_DIR%\IxMilia.Dxf.csproj
-dotnet restore "%LIBRARY_PROJECT%"
-if errorlevel 1 goto error
-dotnet build "%LIBRARY_PROJECT%"
+dotnet restore "%Solution%"
 if errorlevel 1 goto error
 
-:: build and run tests
-set TEST_PROJECT=%~dp0src\IxMilia.Dxf.Test\IxMilia.Dxf.Test.csproj
-dotnet restore "%TEST_PROJECT%"
+dotnet build "%Solution"%
 if errorlevel 1 goto error
-dotnet build "%TEST_PROJECT%"
+
+dotnet test "%Solution"%
 if errorlevel 1 goto error
-dotnet test "%TEST_PROJECT%"
-if errorlevel 1 goto error
+
 goto :eof
 
 :error
